34273ce874eec96b32a8c24cded6d19e1f6c110e
DCZ <dennischenxi@gmail.com>
2024-09-04 13:49:40 +0200

    added image extraction from .mp4 to .hdf5

--- strawml/__init__.py
+++ strawml/__init__.py
@@ -1,2 +1,6 @@
 from strawml.models.model import MyNeuralNet
 from strawml.predict_model import predict
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
