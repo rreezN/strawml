a4d189d1a91e8607ad0a5833ce79d611f2063310
DCZ <dennischenxi@gmail.com>
2024-09-10 13:12:51 +0200

    updated camera record

--- strawml/data/capture_video/main.py
+++ strawml/data/capture_video/main.py
@@ -1,9 +1,31 @@
 from __init__ import *
-from camera_record import record_video
+import cv2
+from video_capture import VideoCaptureAsync
+import time
+from tqdm import tqdm
 
-def main():
-    record_time = 10
-    record_video(record_time)
+
+def record_video(duration, fps=30, data_path='data/raw/videos', byte_limit=1e9):
+    print("STARTED: record_video\n")
+    capture = VideoCaptureAsync(src=0, duration=duration, fps=fps, data_path=data_path, byte_limit=byte_limit)
+    start_time = time.time()
+    # Initialize tqdm with a custom format to show only the elapsed time
+    pbar = tqdm(bar_format='{l_bar}{bar} | Elapsed Time: {elapsed}', ncols=0)
+    capture.start()
+    while True:
+        elapsed_time = time.time() - start_time
+        if elapsed_time >= duration:
+            break
+        pbar.set_postfix_str(f'{elapsed_time:.2f}s')
+    pbar.close()
+    capture.stop()
+    cv2.destroyAllWindows()
+
+    print("\nSTOPPED: record_video")
 
 if __name__ == '__main__':
-    main()
+    duration=10
+    fps=30
+    data_path='data/raw/videos'
+    byte_limit=1e9
+    record_video(duration, fps, data_path, byte_limit)
