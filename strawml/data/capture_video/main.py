d583b26b444fce1856224c5e26e0a608e76dd90f
rreezN <dennischenxi@gmail.com>
2024-09-11 13:54:54 +0200

    updated camera record for RPi deployment - still unstable due to the nature of opencv - contains the default parameters the RPi should be able to run with

--- strawml/data/capture_video/main.py
+++ strawml/data/capture_video/main.py
@@ -8,24 +8,53 @@ import os
 
 def record_video(duration, fps=30, data_path='data/raw/videos', byte_limit=1e9):
     print("STARTED: record_video\n")
-    capture = VideoCaptureAsync(src=1, duration=duration, fps=fps, data_path=data_path, byte_limit=byte_limit)
-    # start_time = time.time()
-    # Initialize tqdm with a custom format to show only the elapsed time
-    # pbar = tqdm(bar_format='{l_bar}{bar} | Elapsed Time: {elapsed}', ncols=0)
+    imgsz = (1024, 768)
+    # imgsz = None
+    capture = VideoCaptureAsync(src=0, imgsz=imgsz)
     capture.start()
-    while capture.started:
-        # elapsed_time = time.time() - start_time
-        # if elapsed_time >= duration:
-            # break
-        # pbar.set_postfix_str(f'{elapsed_time:.2f}s')
+    
+    frame_width = int(capture.get(3))
+    frame_height = int(capture.get(4))
+
+    size = (frame_width, frame_height) 
+    
+    file_index = 0
+    while os.path.exists(f'{data_path}/video_recording_{file_index}.avi'):
+        file_index += 1
+    
+    out = cv2.VideoWriter(f'{data_path}/video_recording_{file_index}.avi', 
+                            cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 
+                            fps, size)
+    
+    n_frames = duration * fps
+    curr_frame = 0
+    pbar = tqdm(total=n_frames, ncols=0)
+    
+    while curr_frame < n_frames:
+        start_time = time.time()
+        grabbed, frame = capture.read()
+        if grabbed:
+            out.write(frame)
+            curr_frame += 1
+            pbar.update(1)
+            if os.path.exists(f'{data_path}/video_recording_{file_index}.avi'):
+                if os.path.getsize(f'{data_path}/video_recording_{file_index}.avi') > byte_limit:
+                    out.release()
+                    file_index += 1
+                    out = cv2.VideoWriter(f'{data_path}/video_recording_{file_index}.avi', 
+                                        cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 
+                                        fps, size)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-    # pbar.close()
+        elapsed_time = time.time() - start_time
+        frame_time = 1.0/fps  # Ideal frame time in seconds
+        if elapsed_time < frame_time:
+            time.sleep(frame_time - elapsed_time) 
+    pbar.close()
     capture.stop()
     cv2.destroyAllWindows()
-
-    print("\nSTOPPED: record_video")
+    print("FINISHED: record_video\n")
 
 def main(args: argparse.Namespace):
     if args.mode == 'record':
@@ -41,16 +70,26 @@ def main(args: argparse.Namespace):
         duration = frame_count / fps
         print(f'Video duration: {duration:.2f}s')
         print(f'Number of frames: {frame_count}')
+        # play the video
+        if args.play_video:
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                cv2.imshow('frame', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
         cap.release()
 
 def argparser():
     parser = argparse.ArgumentParser(description='Record video from webcam')
     parser.add_argument('mode', type=str, choices=['record', 'validate'], help='Mode to run the script in')
-    parser.add_argument('--duration', type=int, default=10, help='Duration of the video in seconds')
-    parser.add_argument('--fps', type=int, default=30, help='Frames per second')
-    parser.add_argument('--data_path', type=str, default='data/raw/videos', help='Path to save the video')
+    parser.add_argument('--duration', type=int, default=1800, help='Duration of the video in seconds')
+    parser.add_argument('--fps', type=int, default=15, help='Frames per second')
+    parser.add_argument('--data_path', type=str, default='/home/dnd/Documents/capture_video/videos/', help='Path to save the video')
     parser.add_argument('--byte_limit', type=float, default=1e9, help='Maximum size of the video file in bytes')
     parser.add_argument('--vid_name', type=str, default='video_recording_0.avi', help='Name of the video file')
+    parser.add_argument('--play_video', action='store_true', help='Play the video after validation')
     return parser.parse_args()
 
 if __name__ == '__main__':
--- strawml/data/capture_video/video_capture.py
+++ strawml/data/capture_video/video_capture.py
@@ -7,24 +7,22 @@ from tqdm import tqdm
 
 # Define video capture class
 class VideoCaptureAsync:
-    def __init__(self, duration, fps, src=0, driver=None, data_path='data/raw/videos', byte_limit=1e9):
+    def __init__(self, src=0, imgsz=None, driver=None):
         self.src = src
         if driver is None:
             self.cap = cv2.VideoCapture(self.src)
         else:
             self.cap = cv2.VideoCapture(self.src, driver)
-        self.cap.set(cv2.CAP_PROP_FPS, fps)
-
-        self.duration = duration
-        self.fps = fps
-        self.data_path = data_path
+            
+        if type(imgsz) is tuple:
+            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, imgsz[0])
+            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, imgsz[1])
+        
         self.grabbed, self.frame = self.cap.read()
         self.started = False
         self.read_lock = threading.Lock()
         self.thread = None
-        self.out = None
-        self.byte_limit = byte_limit # 1e9 bytes = 1 GB # 1e6 bytes = 1 MB
-
+        
     def get(self, var1):
         return self.cap.get(var1)
 
@@ -41,56 +39,21 @@ class VideoCaptureAsync:
         return self
 
     def update(self):
-        
-        frame_width = int(self.cap.get(3))
-        frame_height = int(self.cap.get(4))
-
-        size = (frame_width, frame_height) 
-        
-        file_index = 0
-        while os.path.exists(f'{self.data_path}/video_recording_{file_index}.avi'):
-            file_index += 1
-        
-        self.out = cv2.VideoWriter(f'{self.data_path}/video_recording_{file_index}.avi', 
-                                cv2.VideoWriter_fourcc(*'MJPG'), 
-                                self.fps, size)
-        
-        n_frames = self.duration * self.fps
-        curr_frame = 0
-        pbar = tqdm(total=n_frames, ncols=0)
-        
-        while curr_frame < n_frames:
+        while self.started:
             grabbed, frame = self.cap.read()
             with self.read_lock:
                 self.grabbed = grabbed
                 self.frame = frame
-            if grabbed:
-                self.out.write(frame)
-                curr_frame += 1
-                pbar.update(1)
-
-            # Check file size
-            if os.path.exists(f'{self.data_path}/video_recording_{file_index}.avi'):
-                if os.path.getsize(f'{self.data_path}/video_recording_{file_index}.avi') > self.byte_limit:  # 1e9 bytes = 1 GB # 1e6 bytes = 1 MB
-                    # Close current file
-                    self.out.release()
-                    # Open new file with incremented index
-                    file_index += 1
-                    self.out = cv2.VideoWriter(f'{self.data_path}/video_recording_{file_index}.avi', 
-                                        cv2.VideoWriter_fourcc(*'MJPG'), 
-                                        self.fps, size)
-        self.started = False
 
     def read(self):
         with self.read_lock:
             frame = self.frame.copy()
             grabbed = self.grabbed
-        return grabbed, frame, self.started
+        return grabbed, frame
 
     def stop(self):
         self.started = False
-        self.out.release()
         self.thread.join()
-
+    
     def __exit__(self, exec_type, exc_value, traceback):
         self.cap.release()
