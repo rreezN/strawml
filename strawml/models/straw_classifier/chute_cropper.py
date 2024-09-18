015b7290eb03c07625d4e57466fb5abcc55ab856
DavidAriOstenfeldt <davidostenfeldt@hotmail.com>
2024-09-18 11:59:59 +0200

    added rotation and cropping to bbox

--- strawml/models/straw_classifier/chute_cropper.py
+++ strawml/models/straw_classifier/chute_cropper.py
@@ -5,6 +5,8 @@ import numpy as np
 import torch
 from skimage import transform
 import matplotlib.pyplot as plt
+import cv2
+
 from strawml.data.make_dataset import decode_binary_image
 
 def rotate_point(x, y, cx, cy, angle_radians):
@@ -121,23 +123,68 @@ def rotate_image_and_bbox(image, image_diff, bbox, angle_degrees):
 def rotate_to_bbox(image, bbox) -> tuple:
     x1, y1, x2, y2, x3, y3, x4, y4 = bbox
 
-    # Find the rotation angle of the bounding box
-    dy = y4 - y2
-    dx = x4 - x2
-    m = dy/dx
-    angle = np.arctan(m)
-    d = angle - 45
+    # Calculate the angle of the bounding box
+    angle = np.arctan2(y1 - y4, x1 - x4)
+    angle_degrees = np.rad2deg(angle)
     
     # Rotate the image and bounding box
-    rotated_image, _, rotated_bbox = rotate_image_and_bbox(image, image, bbox, angle_degrees=d)
+    rotated_image, _, rotated_bbox = rotate_image_and_bbox(image, image, bbox, angle_degrees=angle_degrees)
     
     return rotated_image, rotated_bbox
 
 
-def visualize_image_and_bbox(image, bbox) -> None:
-    plt.figure(figsize=(8, 8))
-    plt.imshow(image)
-    plt.plot([bbox[0], bbox[2], bbox[4], bbox[6], bbox[0]], [bbox[1], bbox[3], bbox[5], bbox[7], bbox[1]], 'r-')
+def crop_to_bbox(rotated_image, rotated_bbox) -> tuple:
+    cropped_image = rotated_image.copy()
+    cropped_bbox = rotated_bbox.copy()
+    
+    # Get the bounding box coordinates
+    x1, y1, x2, y2, x3, y3, x4, y4 = rotated_bbox
+    
+    # Get the width and height of the bounding box
+    width = max(x1, x2, x3, x4) - min(x1, x2, x3, x4)
+    height = max(y1, y2, y3, y4) - min(y1, y2, y3, y4)
+    
+    # Crop the image to the bounding box
+    cropped_image = cropped_image[int(min(y1, y2, y3, y4)):int(min(y1, y2, y3, y4) + height), int(min(x1, x2, x3, x4)):int(min(x1, x2, x3, x4) + width)]
+    
+    # Update the bounding box coordinates
+    cropped_bbox[0::2] -= min(x1, x2, x3, x4)
+    cropped_bbox[1::2] -= min(y1, y2, y3, y4)
+    
+    return cropped_image, cropped_bbox
+
+
+def rotate_and_crop_to_bbox(image, bbox) -> tuple:
+    rotated_image, rotated_bbox = rotate_to_bbox(image, bbox)
+    cropped_image, cropped_bbox = crop_to_bbox(rotated_image, rotated_bbox)
+    
+    return cropped_image, cropped_bbox
+
+
+def visualize_image_and_bbox(original_image, original_bbox, rotated_image, rotated_bbox, cropped_image, cropped_bbox) -> None:
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+    axes[0].imshow(original_image)
+    axes[0].plot([original_bbox[0], original_bbox[2], original_bbox[4], original_bbox[6], original_bbox[0]], 
+                 [original_bbox[1], original_bbox[3], original_bbox[5], original_bbox[7], original_bbox[1]], 
+                 'g-')
+    axes[0].set_title('Original Image')
+    axes[0].axis('off')
+    
+    axes[1].imshow(rotated_image)
+    axes[1].plot([rotated_bbox[0], rotated_bbox[2], rotated_bbox[4], rotated_bbox[6], rotated_bbox[0]],
+                 [rotated_bbox[1], rotated_bbox[3], rotated_bbox[5], rotated_bbox[7], rotated_bbox[1]], 
+                 'g-')
+    axes[1].set_title('Rotated Image')
+    axes[1].axis('off')
+    
+    axes[2].imshow(cropped_image)
+    axes[2].plot([cropped_bbox[0], cropped_bbox[2], cropped_bbox[4], cropped_bbox[6], cropped_bbox[0]], 
+                 [cropped_bbox[1], cropped_bbox[3], cropped_bbox[5], cropped_bbox[7], cropped_bbox[1]],
+                 'g-')
+    axes[2].set_title('Cropped Image')
+    axes[2].axis('off')
+    
+    plt.tight_layout()
     plt.show()
     
     
@@ -148,13 +195,21 @@ if __name__ == '__main__':
     # Load the image and bounding box
     hf = h5py.File('data/interim/chute_detection.hdf5', 'r')
     frames = list(hf.keys())
-    image = decode_binary_image(hf[frames[0]]['image'][...])
-    bbox = hf[frames[0]]['annotations']['bbox_chute'][...]
+    if len(frames) < 1:
+        raise ValueError('Need at least one frame to demonstrate rotation.')
+    frame = frames[0]
+    image = decode_binary_image(hf[frame]['image'][...])
+    bbox = hf[frame]['annotations']['bbox_chute'][...]
     
-    # Rotate the image to the bounding box
+    # Rotate and crop the image to the bounding box
     rotated_image, rotated_bbox = rotate_to_bbox(image, bbox)
+    cropped_image, cropped_bbox = crop_to_bbox(rotated_image, rotated_bbox)
     
-    visualize_image_and_bbox(rotated_image, rotated_bbox)
+    visualize_image_and_bbox(image, bbox, rotated_image, rotated_bbox, cropped_image, cropped_bbox)
+    
+    # Combined into one step: Rotate and crop the image to the bounding box
+    # cropped_image, cropped_bbox = rotate_and_crop_to_bbox(image, bbox)
+    
 
 
 
