3f505de715cb237010a182b4412039e2f454ed42
DavidAriOstenfeldt <davidostenfeldt@hotmail.com>
2024-09-23 12:50:37 +0200

    train on cropped and rotated images (to bbox)

--- strawml/train_classifier.py
+++ strawml/train_classifier.py
@@ -1,9 +1,11 @@
 import argparse
 import torch
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 import data.dataloader as dl
 import strawml.models.straw_classifier.cnn_classifier as cnn
+import strawml.models.straw_classifier.chute_cropper as cc
 
 
 def train_model(args, model: torch.nn.Module, train_loader: torch.utils.data.Dataset, val_loader: torch.utils.data.Dataset) -> None:
@@ -19,12 +21,14 @@ def train_model(args, model: torch.nn.Module, train_loader: torch.utils.data.Dat
     
     for epoch in range(args.epochs):
         train_iterator = tqdm(train_loader, unit="batch", position=0, leave=False)
-        train_iterator.set_description(f'Epoch {epoch+1}/{args.epochs}')
+        train_iterator.set_description(f'Training Epoch {epoch+1}/{args.epochs}')
         model.train()
         for (data, target) in train_iterator:
-            
+            # TODO
+            # We need to handle this properly in the dataloader to be able to batch
+            # Instead of tuples, we need to return tensors, e.g frame_data channels = image_rgb + image_heatmap
             frame_data = data[0]
-            fullness = target[2]
+            fullness = target[2][0]
             
             if torch.cuda.is_available():
                 frame_data = frame_data.cuda()
@@ -46,9 +50,13 @@ def train_model(args, model: torch.nn.Module, train_loader: torch.utils.data.Dat
         with torch.no_grad():
             correct = 0
             total = 0
-            for data, target in val_loader:
+            val_iterator = tqdm(val_loader, unit="batch", position=0, leave=False)
+            val_iterator.set_description(f'Validating Epoch {epoch+1}/{args.epochs}')
+            for data, target in val_iterator:
+                # TODO:
+                # Also fix this once dataloader has been fixed
                 frame_data = data[0]
-                fullness = target[2]
+                fullness = target[2][0]
                 
                 if torch.cuda.is_available():
                     frame_data = frame_data.cuda()
@@ -59,7 +67,7 @@ def train_model(args, model: torch.nn.Module, train_loader: torch.utils.data.Dat
                 total += 1
                 _, target_fullness = torch.max(fullness, 0)
                 correct += (predicted == target_fullness)
-                
+            correct = correct.detach().cpu()
             print(f'Epoch: {epoch+1}, Validation Accuracy: {100*correct/total:.2f}%')
             
     return
@@ -69,12 +77,12 @@ def get_args():
     """Get the arguments for the training script.
     """
     parser = argparse.ArgumentParser(description='Train the CNN classifier model.')
-    parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training')
+    parser.add_argument('--batch_size', type=int, default=1, help='Batch size for training')
     parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train for')
     parser.add_argument('--lr', type=float, default=0.001, help='Learning rate for training')
     parser.add_argument('--model_path', type=str, default='models/cnn_classifier.pth', help='Path to load the model from')
     parser.add_argument('--save_path', type=str, default='models/cnn_classifier_new.pth', help='Path to save the model to')
-    parser.add_argument('--data_path', type=str, default='data/processed/chute_detection.hdf5', help='Path to the training data')
+    parser.add_argument('--data_path', type=str, default='data/processed/augmented/chute_detection.hdf5', help='Path to the training data')
     parser.add_argument('--inc_heatmap', type=bool, default=True, help='Include heatmaps in the training data')
     parser.add_argument('--seed', type=int, default=42, help='Random seed for reproducibility')
     
@@ -83,8 +91,11 @@ def get_args():
 if __name__ == '__main__':
     args = get_args()
     
-    train_loader = dl.Chute(data_path=args.data_path, data_type='train', inc_heatmap=args.inc_heatmap, random_state=args.seed)
-    test_loader = dl.Chute(data_path=args.data_path, data_type='test', inc_heatmap=args.inc_heatmap, random_state=args.seed)
+    train_set = dl.Chute(data_path=args.data_path, data_type='train', inc_heatmap=args.inc_heatmap, random_state=args.seed, force_update_statistics=False, data_purpose='straw')
+    test_set = dl.Chute(data_path=args.data_path, data_type='test', inc_heatmap=args.inc_heatmap, random_state=args.seed, force_update_statistics=False, data_purpose='straw')
+    
+    train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False)
     
     cnn_model = cnn.CNNClassifier()
     
