7bb45da7db0b9e19680e618dae9aa2907d9d63f6
DavidAriOstenfeldt <davidostenfeldt@hotmail.com>
2024-09-30 16:36:35 +0200

    distribution plot now shows augmented and original split

--- strawml/data/analysis.py
+++ strawml/data/analysis.py
@@ -22,7 +22,7 @@ def get_frames_by_class(frames: h5py.File) -> dict:
     """Get a dictionary of frames by class.
     """
     print('Sorting frames by class...')
-    frames_by_class = {}
+    frames_by_class = {"augmented": {}, "original": {}}
     frame_names = list(frames.keys())
     
     print("Removing all cropped, rotated or translated images from the dataset (data_purpose='straw level monitoring')")
@@ -33,14 +33,22 @@ def get_frames_by_class(frames: h5py.File) -> dict:
             augmentations = frames[frame_name].attrs['augmented']
             if any(x in augmentations for x in banned_augmentations):
                 frame_names.remove(frame_name)
+            else:
+                current_class = float(frames[frame_name]['annotations']['fullness'][...])
+                frames_by_class['augmented'][current_class] = frames_by_class['augmented'].get(current_class, []) + [frame_name]
+        else:
+            current_class = float(frames[frame_name]['annotations']['fullness'][...])
+            frames_by_class['original'][current_class] = frames_by_class['original'].get(current_class, []) + [frame_name]
+        
     
     
     # print(frames[frame_names[0]]['annotations']['fullness'][...])
-    for frame_name in frame_names:
-        current_class = float(frames[frame_name]['annotations']['fullness'][...])
-        frames_by_class[current_class] = frames_by_class.get(current_class, []) + [frame_name]
+    # for frame_name in frame_names:
+    #     current_class = float(frames[frame_name]['annotations']['fullness'][...])
+    #     frames_by_class[current_class] = frames_by_class.get(current_class, []) + [frame_name]
     
-    frames_by_class = dict(sorted(frames_by_class.items()))
+    frames_by_class['augmented'] = dict(sorted(frames_by_class['augmented'].items()))
+    frames_by_class['original'] = dict(sorted(frames_by_class['original'].items()))
     
     # for key in frames_by_class.keys():
     #     print(f'Class {key}: {len(frames_by_class[key])} frames')
@@ -52,28 +60,42 @@ def plot_class_distribution(class_dict: dict, frames: h5py.File) -> None:
     """Plot the class distribution of the dataset.
     """
     print("Plotting class distribution...")
-    classes = list([int(x*100) for x in class_dict.keys()])
-    counts = [len(class_dict[key]) for key in class_dict.keys()]
+    classes_original = list([int(x*100) for x in class_dict['original'].keys()])
+    classes_augmented = list(int(x*100) for x in class_dict['augmented'].keys())
+    classes = list(set(classes_original) | set(classes_augmented))
     
-    total_frames = sum(counts)
+    counts_original = [len(class_dict['original'].get(float(key/100), [])) for key in classes]
+    counts_augmented = [len(class_dict['augmented'].get(float(key/100), [])) for key in classes]
+    
+    counts = {
+        'original': counts_original, 
+        'augmented': counts_augmented,
+        }
+    
+    total_frames = sum(counts_augmented + counts_original)
     
     # Create a 2x11 figure with bar chart in top left corner, and examples from each class in the remaining slots
     fig = plt.figure(figsize=(14,7))
     gs = gridspec.GridSpec(2, 13, figure=fig, wspace=0.3, hspace=0.3)
     
     ax1 = fig.add_subplot(gs[0, 0:2])
-    ax1.barh(classes, counts, align='center', height=4)
+    ax1.grid(axis='x', linestyle='--', alpha=0.5)
+    
+    bottom = np.zeros(len(classes))
+    left = np.zeros(len(classes))
+    for data_type, count in counts.items():
+        ax1.barh(classes, count, align='center', label=data_type, height=4, left=left)
+        left += count
+    
+    ax1.legend()
     ax1.set_yticks(np.arange(0, max(classes*100) + 1, 10))
     ax1.set_title('Class Distribution')
     ax1.set_xlabel('Count')
     ax1.set_ylabel('Class')
     ax1.invert_yaxis()
-    ax1.set_xlim(0, max(counts) + 10)
+    ax1.set_xlim(0, max(np.array(counts['original']) + np.array(counts['augmented'])) + 10)
 
-    image_index = np.random.randint(0, len(class_dict[float(classes[0]/100)]))
-    first_image = decode_binary_image(frames[class_dict[float(classes[0]/100)][image_index]]['image'][...])
-    first_image = cv2.cvtColor(first_image, cv2.COLOR_BGR2RGB)
-    first_image, bbox = rotate_and_crop_to_bbox(first_image, frames[class_dict[float(classes[0]/100)][image_index]]['annotations']['bbox_chute'][...])
+    first_image = load_image(class_dict, classes, 0)
     ax2 = fig.add_subplot(gs[0, 2])
     ax2.imshow(first_image)
     ax2.set_xticks([])
@@ -89,11 +111,8 @@ def plot_class_distribution(class_dict: dict, frames: h5py.File) -> None:
     class_counter = 1
     for row in range(2):
         for col in range(3, 13):
-            image_index = np.random.randint(0, len(class_dict[float(classes[class_counter]/100)]))
             ax = fig.add_subplot(gs[row, col])
-            image = decode_binary_image(frames[class_dict[float(classes[class_counter]/100)][image_index]]['image'][...])
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            image, bbox = rotate_and_crop_to_bbox(image, frames[class_dict[float(classes[class_counter]/100)][image_index]]['annotations']['bbox_chute'][...])
+            image = load_image(class_dict, classes, class_counter)
             image = cv2.resize(image, (204, 1370))
             ax.imshow(image)
             ax.set_title(f'Class {classes[class_counter]}')
@@ -104,26 +123,21 @@ def plot_class_distribution(class_dict: dict, frames: h5py.File) -> None:
     
     
     
-    plt.suptitle(f'Class Distribution, Examples and t-SNE ({frames.filename})')
+    plt.suptitle(f'Class Distribution and Examples ({frames.filename})')
     plt.show()
 
 
-def calculate_tsne(features: np.ndarray) -> np.ndarray:
-    """Calculate the t-SNE representation of the images.
-    """
-    print('Calculating t-SNE representation...')
-    tsne = skm.TSNE(n_components=2, random_state=42)
-    tsne_representation = tsne.fit_transform(features)
-    
-    return tsne_representation
-
-def normalize_tsne(x: np.ndarray) -> np.ndarray:
-    """Normalize the array.
-    """
-    value_range = np.max(x) - np.min(x)
-    starts_from_zero = x - np.min(x)
-    return starts_from_zero / value_range
-    
+def load_image(class_dict, classes, class_number) -> np.ndarray:
+    augmented = np.random.randint(0, 2) == 1
+    if augmented and class_dict['augmented'].get(float(classes[class_number]/100), None) is not None:
+        image_index = np.random.randint(0, len(class_dict['augmented'][float(classes[class_number]/100)]))
+        first_image = decode_binary_image(frames[class_dict['augmented'][float(classes[class_number]/100)][image_index]]['image'][...]) 
+        first_image, bbox = rotate_and_crop_to_bbox(first_image, frames[class_dict['augmented'][float(classes[class_number]/100)][image_index]]['annotations']['bbox_chute'][...])
+    else:
+        image_index = np.random.randint(0, len(class_dict['original'][float(classes[class_number]/100)]))
+        first_image = decode_binary_image(frames[class_dict['original'][float(classes[class_number]/100)][image_index]]['image'][...])
+        first_image, bbox = rotate_and_crop_to_bbox(first_image, frames[class_dict['original'][float(classes[class_number]/100)][image_index]]['annotations']['bbox_chute'][...])
+    return cv2.cvtColor(first_image, cv2.COLOR_BGR2RGB)
 
 if __name__ == '__main__':
     frames = h5py.File('data/processed/augmented/chute_detection.hdf5', 'r')
