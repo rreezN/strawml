7ce2cda32fa982e0c498138779c779311f86beee
DavidAriOstenfeldt <davidostenfeldt@hotmail.com>
2024-11-04 13:08:13 +0100

    update sh

--- cross_validate.sh
+++ cross_validate.sh
@@ -2,7 +2,7 @@
 #BSUB -J straw_classifier
 #BSUB -o straw_classifier%J.out
 #BSUB -e straw_classifier%J.err
-#BSUB -q gpua100
+#BSUB -q cabgpu
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 4
 #BSUB -R "rusage[mem=8G]"
@@ -23,4 +23,4 @@ module load matplotlib/3.8.3-numpy-1.26.4-python-3.10.13
 # NOTE: needs to have been built with the same numpy / SciPy  version as above!
 source ~/strawml/.venv/bin/activate
 
-python3 strawml/train_straw_model.py --data_path data/interim/chute_detection.hdf5 --lr 0.00001 --epochs 100 --cont
+python3 strawml/cross_validate_straw_model.py --data_path data/interim/chute_detection.hdf5 --lr 0.00001 --epochs 25 --cont --folds 5
