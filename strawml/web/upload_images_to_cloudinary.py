3a41689fad4c28877084e3a9babf6332275eebd7
DavidAriOstenfeldt <davidostenfeldt@hotmail.com>
2024-12-20 14:06:07 +0100

    upload upright images to cloudinary

--- strawml/web/upload_images_to_cloudinary.py
+++ strawml/web/upload_images_to_cloudinary.py
@@ -44,11 +44,11 @@ def upload_to_cloudinary(cloudinary_txt: str, image_folder: str, tags: str = '')
             upload_result = cloudinary.uploader.upload(f'{image_folder}/{images_to_upload[i]}', asset_folder='strawml', public_id=id, overwrite=True, resource_type='image')
         tqdm_images.set_postfix({'url': upload_result["secure_url"]})
         
-        if i > 5:
-            break
+        # if i > 5:
+        #     break
     
     tqdm_images.close()
     print('Upload complete!')
 
 if __name__ == '__main__':
-    upload_to_cloudinary('data/cloudinary.txt', 'data/processed/recordings', tags=['upright', 'weak_smoothing'])
+    upload_to_cloudinary('data/cloudinary.txt', 'data/processed/recordings', tags=['upright', 'weak_smoothing', "bugged_line"])
