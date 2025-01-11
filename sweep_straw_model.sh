60f2df78d5001a4f1eba75d499e56cdafbd79b8c
DavidAriOstenfeldt <davidostenfeldt@hotmail.com>
2025-01-11 23:16:35 +0100

    [SYNC] update sweep config

--- sweep_straw_model.sh
+++ sweep_straw_model.sh
@@ -2,7 +2,7 @@
 #BSUB -J straw_classifier
 #BSUB -o straw_classifier%J.out
 #BSUB -e straw_classifier%J.err
-#BSUB -q gpuv100
+#BSUB -q gpua100
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 4
 #BSUB -R "rusage[mem=16G]"
@@ -23,4 +23,4 @@ module load matplotlib/3.8.3-numpy-1.26.4-python-3.10.13
 # NOTE: needs to have been built with the same numpy / SciPy  version as above!
 source ~/strawml/.venv/bin/activate
 
-wandb agent meliora/straw-sweep/6e7wbi89 --count 2
+wandb agent meliora/straw-sweep/putbdm26 --count 2
