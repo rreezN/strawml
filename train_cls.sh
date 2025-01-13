44d79ec6d6750c8635dc285697dfbfaed6005fbc
DavidAriOstenfeldt <davidostenfeldt@hotmail.com>
2025-01-13 10:56:17 +0100

    [SYNC] setup new hyperparams

--- train_cls.sh
+++ train_cls.sh
@@ -24,4 +24,4 @@ module load matplotlib/3.8.3-numpy-1.26.4-python-3.10.13
 # NOTE: needs to have been built with the same numpy / SciPy  version as above!
 source ~/Documents/strawml/venv/bin/activate
 
-python3 strawml/train_straw_model.py --model convnextv2 --load_model models/convnextv2 --batch_size 12 --lr 0.0000446831361387714 --image_size 672 208 --id new_best_convnextv2 --num_hidden_layers 3 --use_sigmoid --balanced_dataset --data_subsample 1.0 --optim adam --augment_probability 0.0 --cont --use_wce --hpc --epochs 150 --pretrained --data_path train.hdf5
+python3 strawml/train_straw_model.py --model convnextv2 --load_model models/convnextv2 --batch_size 12 --lr 0.00008066450967358205 --image_size 672 208 --id best_sweep_98 --num_hidden_layers 3 --use_sigmoid --balanced_dataset --data_subsample 1.0 --optim adam --augment_probability 0.0 --cont --use_wce --hpc --epochs 150 --pretrained --data_path train.hdf5
