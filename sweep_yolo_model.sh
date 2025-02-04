65dfc42dd4a8545837f58fdd0907ceae2e7031b0
Dennis <s194247@student.dtu.dk>
2025-02-04 23:54:40 +0100

    [NEW] Cross validate now with test metrics and deleting local yolo save files after run

--- sweep_yolo_model.sh
+++ sweep_yolo_model.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#BSUB -J yolo_sweeper_chute
-#BSUB -o yolo_sweeper_chute%J.out
-#BSUB -e yolo_sweeper_chute%J.err
-#BSUB -q gpuv100
+#BSUB -J yolo_sweeper_straw
+#BSUB -o yolo_sweeper_straw%J.out
+#BSUB -e yolo_sweeper_straw%J.err
+#BSUB -q gpua10
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"
@@ -24,6 +24,5 @@ module load matplotlib/3.8.3-numpy-1.26.4-python-3.10.13
 # NOTE: needs to have been built with the same numpy / SciPy  version as above!
 source venv/bin/activate
 
-# python strawml/cross_validate_yolo.py --path /work3/s194247/yolo_format_bbox_straw_whole_5fold --id 4ccyh2d4
-# python strawml/cross_validate_yolo.py --path /work3/s194247/yolo_format_bbox_straw_5fold --id 872t9e6k
-python strawml/cross_validate_yolo.py --path /work3/s194247/yolo_format_bbox_chute_5fold --id xkh5pghf
+python strawml/cross_validate_yolo.py --path /work3/s194247/yolo_format_bbox_chute_whole_4fold --test_data_yaml /work3/s194247/test_data_set.yaml --id tbma63io # straw whole
+# python strawml/cross_validate_yolo.py --path /work3/s194247/yolo_format_bbox_chute_whole_4fold --test_data_yaml /work3/s194247/test_data_set.yaml --id 5rj3uy0z # chute whole
