2be690cedc4a2c5bce55165443f9869858d47780
DavidAriOstenfeldt <davidostenfeldt@hotmail.com>
2025-02-06 21:34:57 +0100

    [SYNC]

--- train_cls.sh
+++ train_cls.sh
@@ -24,4 +24,4 @@ module load matplotlib/3.8.3-numpy-1.26.4-python-3.10.13
 # NOTE: needs to have been built with the same numpy / SciPy  version as above!
 source ~/strawml/.venv/bin/activate
 
-python3 strawml/train_straw_model.py --model convnext --load_model models/convnext --seed 42 --batch_size 4 --lr 0.00001120659857537586 --image_size 672 208 --id best_convnext_seed42 --data_subsample 1.0 --optim adam --augment_probability 0.0 --cont --use_wce --hpc --epochs 150 --pretrained --data_path train.hdf5
+python3 strawml/train_straw_model.py --model convnext --load_model models/convnext --seed 0 --batch_size 4 --lr 0.00001120659857537586 --image_size 672 208 --id best_convnext_:apriltag_seed0 --data_subsample 1.0 --optim adam --augment_probability 0.0 --cont --use_wce --hpc --epochs 150 --pretrained --data_path train.hdf5 --cutout_type apriltag
